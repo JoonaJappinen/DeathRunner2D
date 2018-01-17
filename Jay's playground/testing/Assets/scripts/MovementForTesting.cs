a86918bf78c1b5a98366b4a0e48ba6c462e21dab
JoonaJappinen <jappinen.joona@gmail.com>
2018-01-17 11:41:54 +0200

    Added Jay's Playground
    :)

---
+++ Jay's playground/testing/Assets/scripts/MovementForTesting.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementForTesting : MonoBehaviour {
+
+    Rigidbody2D rd2d;
+    bool isGrounded = true;
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpPower;
+
+	void Start ()
+    {
+        speed = 20;
+        jumpPower = 250;
+        rd2d = GetComponent<Rigidbody2D>();	
+	}
+
+	void Update ()
+    {
+        if (Input.GetKey("space") && isGrounded)
+        {
+            rd2d.AddForce(transform.up * jumpPower);
+        }
+        else if(Input.GetKey("left"))
+        {
+            rd2d.AddForce(transform.right * speed * -1);
+        }
+        else if(Input.GetKey("right"))
+        {
+            rd2d.AddForce(transform.right * speed);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
---
+++ Jay's playground/testing/Assets/scripts/hpAndDmg.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hpAndDmg : MonoBehaviour {
+
+    [SerializeField]
+    private int hp;
+    private int dmg;
+
+    bool isAlive = true;
+
+	// Use this for initialization
+	void Start ()
+    {
+        hp = 3;
+        dmg = 1;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(hp <= 0)
+        {
+            isAlive = false;
+        }
+
+        if(isAlive == false)
+        {
+            Destroy(gameObject);
+        }
+
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "killBox")
+        {
+            hp -= dmg;
+            Debug.Log("HP: " + hp);
+        }
+    }
+}

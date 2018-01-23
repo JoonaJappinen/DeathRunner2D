d159570d04fcf2d71b3e7f35e5ece652cc6a996e
JoonaJappinen <jappinen.joona@gmail.com>
2018-01-23 16:19:11 +0200

    Had a discussion on level spawning

--- Jay's playground/testing/Assets/scripts/LevelSpawning.cs
+++ Jay's playground/testing/Assets/scripts/LevelSpawning.cs
@@ -6,11 +6,18 @@ public class LevelSpawning : MonoBehaviour {
 
 
     public Transform prefab;
+
+    public float pancake = 3;
     // Use this for initialization
     void Start ()
     {
-        Instantiate(prefab);	
-	}
+        //Instantiate(prefab);	
+
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 20.0F, pancake, 0), Quaternion.identity);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
